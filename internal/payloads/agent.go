44b9758e1f3a5575bc62499802da1086cb388338
afif0808 <afif.panai@gmail.com>
2021-10-15 09:08:41 +0700

    Add new payload and fill empty struct

--- internal/payloads/agent.go
+++ internal/payloads/agent.go
@@ -1,8 +1,15 @@
 package payloads
 
 type candidateAgent struct {
+	ID int64 `json:"id"`
 }
 
 type QiscusAgentAllocation struct {
+	RoomID    int64          `json:"room_id"`
 	Candidate candidateAgent `json:"candidate_agent"`
+}
+
+type QiscusAgentAssignment struct {
+	RoomID  int64
+	AgentID int64
 }
