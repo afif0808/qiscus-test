7522943df6d35e4159344558b00260d8a847426d
root <afif.panai@gmail.com>
2021-10-16 15:01:06 +0700

    Set up Heroku

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module github.com/afif0808/qiscus-test
 
 go 1.17
 
-require github.com/labstack/echo v3.3.10+incompatible
-
 require (
+	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	agemtmodule "github.com/afif0808/qiscus-test/internal/modules/qiscus/agent"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Panic(err)
+	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct {
+			Message string `json:"message"`
+		}{Message: "Hello World"})
+	})
+	agemtmodule.InjectAgentModule(e)
+
+	port := os.Getenv("PORT")
+	e.Start(":" + port)
 
 }
